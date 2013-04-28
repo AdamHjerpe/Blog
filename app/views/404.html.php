81c69a0b7799034c19f9aeb4bc68668f3b69e5c7
AdamHjerpe <Adam_Hjerpe@hotmail.com>
2013-04-28 20:01:24 +0200

    General update and fixes
    Signed-off-by: AdamHjerpe <Adam_Hjerpe@hotmail.com>

--- app/views/404.html.php
+++ app/views/404.html.php
@@ -4,13 +4,10 @@
   <meta charset="utf-8" />
 
   <title>404 Not Found | <?php echo config('blog.title') ?></title>
-  <link href="<?php echo site_url() ?>assets/css/style.css" rel="stylesheet" />
-
-  <!-- Include the Open Sans font -->
-  <link href="http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700&subset=latin,cyrillic-ext" rel="stylesheet" />
+  <link href="css/styles.css" rel="stylesheet" />
 
 </head>
-<body>
+<body class="error-404">
 
   <div class="center message">
     <h1>This page doesn't exist!</h1>
