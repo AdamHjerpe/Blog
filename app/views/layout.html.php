43146f3536e1ca22a153073225d375ff36c9b591
AdamHjerpe <Adam.Hjerpe@hotmail.com>
2013-06-22 23:13:51 +0200

    Added gaug.es tracking script
    Signed-off-by: AdamHjerpe <Adam.Hjerpe@hotmail.com>

--- app/views/layout.html.php
+++ app/views/layout.html.php
@@ -52,6 +52,19 @@
     <!-- Keep script import at bottom, right before closing </body> tag -->
     <script src="js/highlight.pack.min.js"></script>
     <script>hljs.initHighlightingOnLoad();</script>
+    <script type="text/javascript">
+      var _gauges = _gauges || [];
+      (function() {
+        var t   = document.createElement('script');
+        t.type  = 'text/javascript';
+        t.async = true;
+        t.id    = 'gauges-tracker';
+        t.setAttribute('data-site-id', '51c61296108d7b116700000d');
+        t.src = '//secure.gaug.es/track.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(t, s);
+      })();
+    </script>
     <!--script type="text/javascript" src="js/prettify.min.js"></script-->
   </body>
 </html>
